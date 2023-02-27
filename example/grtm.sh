abe852bcf2e323a39d106f8b2e031ce0589f4cac
core-man <coreman.seism@gmail.com>
2023-02-27 21:47:37 +0800

    Add comments in grtm.sh

--- example/grtm.sh
+++ example/grtm.sh
@@ -1,3 +1,6 @@
-cp ../bin/grtm ../bin/trav .
+#!/usr/bin/env bash
+
+# a distance of 100 km for a surface source
 ./grtm.pl -Mcrust1.0/0 -N10/0.04  100
+# a distance of 100 km for a 10 km depth source
 ./grtm.pl -Mcrust1.0/10 -N10/0.04  100
